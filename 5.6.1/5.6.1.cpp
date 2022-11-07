852cd67bf5ea456b66d0f868886884dd29f84f68
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-11-08 00:39:34 +0300

    f_symbols

---
+++ 5.6.1/5.6.1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+#include <string>
+#include <map>
+#include <utility>
+#include <set>
+#include <algorithm>
+
+int main()
+{    
+    std::map<char,int> text ;
+    std::string a;
+
+    std::cout << "[IN]: ";
+    std::getline(std::cin, a);
+    
+    for (const auto& elem : a) {
+        ++text[elem];
+    }
+    
+    std::map<int, char> reverse_text;
+    for (std::pair<char, int> pair : text) {
+        reverse_text[pair.second] = pair.first;
+    }
+    
+    auto r_i = reverse_text.rbegin();
+    
+    std::cout << "[OUT]:\n";
+    while (r_i!= reverse_text.rend())    {
+        std::cout << r_i->second << ": " << r_i->first << '\n';
+        r_i++;
+    }
+    
+    return 0;
+}
